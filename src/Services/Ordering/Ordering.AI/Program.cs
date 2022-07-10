6fd77bed46924687793a8a67a34f0ad8a1e9f24b
Nikhil <nikhilbanuri4@gmail.com>
2022-07-10 21:54:13 +0530

    Added references and connection string

--- src/Services/Ordering/Ordering.AI/Program.cs
+++ src/Services/Ordering/Ordering.AI/Program.cs
@@ -1,4 +1,5 @@
 using Ordering.Application;
+using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
-//builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
