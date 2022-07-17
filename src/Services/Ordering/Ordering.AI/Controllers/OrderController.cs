94bad50f1b62cecc8890fe4a79d146064bd80251
Nikhil <nikhilbanuri4@gmail.com>
2022-07-17 13:58:40 +0530

    docker changes are implemented and api route is changed

--- src/Services/Ordering/Ordering.AI/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.AI/Controllers/OrderController.cs
@@ -8,7 +8,7 @@ using System.Net;
 
 namespace Ordering.AI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
     {
